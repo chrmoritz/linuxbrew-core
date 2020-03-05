96e0117beb4899acafab3a2a4f653d2cb8e4b33c
Christian Moritz <chrmoritz@users.noreply.github.com>
2020-03-05 21:05:44 +0100

    Fix deno on Linux

--- Formula/deno.rb
+++ Formula/deno.rb
@@ -11,32 +11,34 @@ class Deno < Formula
     sha256 "036fa380c08bfa1d3490f8a9d151756779c53da79d14010d613f4da9b00b16cc" => :high_sierra
   end
 
-  depends_on :macos # Due to Python 2
   depends_on "llvm" => :build if OS.linux? || DevelopmentTools.clang_build_version < 1100
   depends_on "ninja" => :build
   depends_on "rust" => :build
   unless OS.mac?
     depends_on "xz" => :build
-    depends_on "python@2"
+    depends_on "pypy" => :build # use PyPy2.7 instead of python@2 on Linux
   end
 
   depends_on :xcode => ["10.0", :build] if OS.mac? # required by v8 7.9+
 
-  # Does not work with Python 3
-  # https://github.com/denoland/deno/issues/2893
-  uses_from_macos "python@2"
   uses_from_macos "xz"
 
+  # Use older revision on Linux, newer does not work.
   resource "gn" do
     url "https://gn.googlesource.com/gn.git",
-      :revision => "a5bcbd726ac7bd342ca6ee3e3a006478fd1f00b5"
+      :revision => OS.mac? ? "a5bcbd726ac7bd342ca6ee3e3a006478fd1f00b5" : "152c5144ceed9592c20f0c8fd55769646077569b"
   end
 
   def install
+     # build gn with llvm clang too (g++ is too old)
+    ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
+    # use pypy for Python 2 build scripts
+    ENV["PYTHON"] = Formula["pypy"].opt_bin/"pypy"
+
     # Build gn from source (used as a build tool here)
     (buildpath/"gn").install resource("gn")
     cd "gn" do
-      system "python", "build/gen.py"
+      system Formula["pypy"].opt_bin/"pypy", "build/gen.py"
       system "ninja", "-C", "out/", "gn"
     end
 
@@ -48,10 +50,6 @@ class Deno < Formula
       ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
     else # build with system clang
       ENV["CLANG_BASE_PATH"] = "/usr/"
-    end
-
-    unless OS.mac?
-      system "core/libdeno/build/linux/sysroot_scripts/install-sysroot.py", "--arch=amd64"
     end
 
     cd "cli" do
